71841bbafbe38d5dfcf90b97100c5165377bee88
Rasveer Bansi <rasveerb@sky.com>
2021-05-08 11:37:54 +0100

    created app.js routing

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {Route, Switch } from 'react-router-dom';
+import './App.css';
+
+// import Homepage from './pages/homepage';
+// import Projects from './pages/projects';
+// import Error from './pages/error';
+// import Navbar from './components/ContentElements/Navbar/navbar';
+// import Footer from './components/ContentElements/Footer/footer';
+// import About from './pages/about';
+// import Contact from './pages/contact';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Navbar />
+    <main>
+      <Switch>
+        <Route path="/" component={Homepage} exact />
+        <Route path="/projects" component={Projects} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+        <Route component={Error} />
+      </Switch>
+    </main>
+    <Footer /> */}
+    <h1>HELLOOOOOO</h1>
+    </div>
+  );
+}
+
+export default App;
