076434c083f5782cde7fae3b23ffcd1f31be9525
Rasveer Bansi <rasveerb@sky.com>
2021-05-08 15:12:26 +0100

    added project page template

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import {Route, Switch } from 'react-router-dom';
 import './App.css';
 
 import Homepage from './pages/homepage';
-// import Projects from './pages/projects';
+import Projects from './pages/projects';
 import Error from './pages/error';
 import Navbar from './components/ContentElements/Navbar/navbar';
 import Footer from './components/ContentElements/Footer/footer';
@@ -17,7 +17,7 @@ function App() {
     <main>
       <Switch>
         <Route path="/" component={Homepage} exact />
-        {/* <Route path="/projects" component={Projects} /> */}
+        <Route path="/projects" component={Projects} />
         {/* <Route path="/about" component={About} /> */}
         <Route path="/contact" component={Contact} />
         <Route component={Error} /> 
